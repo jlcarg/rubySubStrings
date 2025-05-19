48446a670e7fbf25ec84053a1d5ac8de4ea01130
Juan L C <202941136+jlcarg@users.noreply.github.com>
2025-05-19 19:42:25 +0900

    Add first version that works with one word
    When passing a phrase with more than one word as an argument, the outcome is not what is expected yet

--- script.rb
+++ script.rb
@@ -1,27 +1,31 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(string, dictionary)
-  string = 'below'
-  string_length = string.length
-  string_into_array = string.split("")
-  string_into_array.reduce(Hash.new(0)) do |hash, letter|
-    letter
+  possible_substrings = separate_possible_substrings string
+  substring_count = possible_substrings.reduce(Hash.new(0)) do |hash, substring|
+    if dictionary.include?(substring)
+      hash[substring] += 1
+      hash
+    else
+      hash
+    end
   end
+  puts substring_count
 end
 
-def separate_possible_substrings2(string)
+def separate_possible_substrings(string)
   string_into_array = string.split ("")
   number_of_letters = string_into_array.size
   possible_substrings = []
   string_into_array.each_with_index do |letter, i|
     (i..number_of_letters).each do |j|
-      p string_into_array[i, j]
-      p string_into_array[i, j] != []
       unless string_into_array[i, j] == [] || possible_substrings.include?(string_into_array[i, j].join(""))
-        p string_into_array[i, j]
         possible_substrings << string_into_array[i, j].join("")
       end
     end
   end
   possible_substrings
 end
+
+substrings("below", dictionary)
+# separate_possible_substrings("Howdy partner, sit down! How's it going?")
